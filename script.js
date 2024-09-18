c6921134b596d7ef75dfa4fe78a88868e9eef4cb
Bang <bangpham10062002@gmail.com>
2024-09-19 03:56:21 +0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const display = document.getElementById("display");
+
+const appendToDisplay = function (input) {
+  display.value += input;
+};
+
+const clearDisplay = function () {
+  display.value = "";
+};
+
+const calculate = function () {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+    setTimeout(() => (display.value = ""), 1000);
+  }
+};
